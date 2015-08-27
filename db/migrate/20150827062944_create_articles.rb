7f3598b277967c756bc0df9a1f840046351da918
Sylvia <sylviakieha@gmail.com>
2015-08-27 09:39:10 +0300

    edited migrate file

--- db/migrate/20150827062944_create_articles.rb
+++ db/migrate/20150827062944_create_articles.rb
@@ -1,7 +1,7 @@
 class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
-      t.srting :title
+      t.string :title
       t.text :body
 
       t.timestamps null: false

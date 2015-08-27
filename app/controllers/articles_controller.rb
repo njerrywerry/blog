d1ed84e8d8186ad2038ae69e8dfd5d49b1bb2469
Sylvia <sylviakieha@gmail.com>
2015-08-27 17:46:12 +0300

    added edit action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,6 +19,10 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  def edit
+    @article = Article.find(params[:id]) 
+  end
+
   def destroy
     @article = Article.find(params[:id])
     @article.destroy

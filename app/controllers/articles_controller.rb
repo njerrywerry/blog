412ca14cd9f8913959a9b35f7a6cb59b61581bb5
Sylvia <sylviakieha@gmail.com>
2015-08-27 18:52:08 +0300

    added flash to create and delete actions, and displayes to app layout

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,6 +16,8 @@ class ArticlesController < ApplicationController
     @article =  Article.new(article_params)
     @article.save
 
+    flash.notice = "Article '#{@article.title}' Created!"
+
     redirect_to article_path(@article)
   end
 
@@ -35,6 +37,9 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
+
+    flash.notice = "Article '#{@article.title}' Deleted!"
+
     redirect_to articles_path
   end
 

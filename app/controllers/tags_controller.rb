0ba274bc4dea8c5706c10e635e63e15a9a980643
Sylvia <sylviakieha@gmail.com>
2015-08-28 12:46:49 +0300

    listed and added delete functionality on tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -7,4 +7,13 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
   end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+
+    flash.notice = "Tag '#{@tag.name}' Deleted!"
+
+    redirect_to tags_path
+  end
 end

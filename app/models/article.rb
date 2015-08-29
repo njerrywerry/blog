c85c01710e3f7c6ef11ffe1644328f69253ebef6
Sylvia <sylviakieha@gmail.com>
2015-08-29 19:08:16 +0300

    added image to article

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ActiveRecord::Base
   has_many :comments
   has_many :taggings
   has_many :tags, through: :taggings
-  has_attached_files :image
+  has_attached_file :image
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
   def tag_list=(tags_string)

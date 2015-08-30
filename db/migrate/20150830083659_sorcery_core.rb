32d4e9a7f313bfb72c718b749371ed57b934f087
Sylvia <sylviakieha@gmail.com>
2015-08-30 11:42:35 +0300

    generated author model with sorcery

---
+++ db/migrate/20150830083659_sorcery_core.rb
@@ -0,0 +1,13 @@
+class SorceryCore < ActiveRecord::Migration
+  def change
+    create_table :authors do |t|
+      t.string :email,            :null => false
+      t.string :crypted_password
+      t.string :salt
+
+      t.timestamps
+    end
+
+    add_index :authors, :email, unique: true
+  end
+end

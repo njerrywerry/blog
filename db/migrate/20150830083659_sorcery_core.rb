424251c1aae7ccdd46c5e4d2c007c367fdf2cebf
Sylvia <sylviakieha@gmail.com>
2015-08-30 11:46:33 +0300

    migrated author model into db

--- db/migrate/20150830083659_sorcery_core.rb
+++ db/migrate/20150830083659_sorcery_core.rb
@@ -1,9 +1,10 @@
 class SorceryCore < ActiveRecord::Migration
   def change
     create_table :authors do |t|
+      t.string :username,         :null => false
       t.string :email,            :null => false
-      t.string :crypted_password
-      t.string :salt
+      t.string :crypted_password, :null => false
+      t.string :salt,             :null => false
 
       t.timestamps
     end

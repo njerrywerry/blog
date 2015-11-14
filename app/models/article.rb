05a3a505c2ecde741dc3b548f2c846e30128290a
Sylvia <sylviakieha@gmail.com>
2015-11-14 16:53:19 +0300

    added validations to articles and comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,8 @@ class Article < ActiveRecord::Base
   has_many :tags, through: :taggings
   has_attached_file :image
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+  validates :title, presence: true
+  validates :body, presence: true
 
   def tag_list=(tags_string)
     tag_names = tags_string.split(",").collect {|s| s.strip.downcase}.uniq
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :article
+  validates :author_name, presence: true
+  validates :body, presence: true
 end

1ffc6ee12a4ed641e019a8c44f08fbb1f217404b
wolfieorama <wachirawamunyi@gmail.com>
2015-11-22 23:40:12 +0300

    added status and jquery to dynamically load dates

--- app/assets/javascripts/articles.coffee
+++ app/assets/javascripts/articles.coffee
@@ -1,3 +1,10 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+change_visibility = (status) ->
+  if status == "Scheduled"
+    $(".published-field").show()
+  else
+    $(".published-field").hide()
+
+jQuery ->
+  change_visibility $("#article_status :selected").text()
+  $("#article_status").on "change", (e) ->
+    change_visibility $(this).find(":selected").text()
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,25 @@
 class Article < ActiveRecord::Base
+  scope :draft, ->{ where(published_at: nil) }
+  scope :published, ->{ where.not(published_at: nil).where("published_at <= ?", Time.zone.now) }
+  scope :scheduled, ->{ where.not(published_at: nil).where("published_at > ?", Time.zone.now) }
+
+  attr_accessor :status
+
+  before_validation :clean_up_status
+
+  def clean_up_status
+    self.published_at = case status
+                        when "Draft"
+                          nil
+                        when "Published"
+                          Time.zone.now
+                        else
+                          published_at
+                        end
+    true
+  end
+
+
   has_many :comments
   has_many :taggings
   has_many :tags, through: :taggings
